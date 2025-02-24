b3914bbe175ea972f80194c98a4d7673b2b695a1
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-02-24 13:32:40 +0900

    Add overlay background on mobile navigation open

--- src/js/index.js
+++ src/js/index.js
@@ -9,11 +9,27 @@ const closeMenu = document.querySelector(
 const navigation = document.querySelector(".header__navigation");
 const navigationList = document.querySelector(".header__navigation-list");
 
+const overlay = document.querySelector(".overlay");
+
+const clonseNavigation = () => {
+  navigation.style = "display: none";
+  overlay.style = "opacity: 0; visibility: hidden;";
+};
+
 hamburgerMenu.addEventListener("click", () => {
   navigation.style = "display: flex";
   navigationList.style = "display: flex";
+  overlay.style = "opacity: 1; visibility: visible";
 });
 
 closeMenu.addEventListener("click", () => {
-  navigation.style = "display: none";
+  clonseNavigation();
+});
+
+overlay.addEventListener("click", () => {
+  clonseNavigation();
+});
+
+window.addEventListener("scroll", () => {
+  clonseNavigation();
 });
