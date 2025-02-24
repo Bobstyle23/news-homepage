75382ccbba2ed8b26362e132fec484c07efca918
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-02-24 12:05:41 +0900

    Add mobile navigation functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
+const hamburgerMenu = document.querySelector(
+  '[aria-controls="primary-navigation"]',
+);
 
+const closeMenu = document.querySelector(
+  '[aria-controls="secondary-navigation"]',
+);
+
+const navigation = document.querySelector(".header__navigation");
+const navigationList = document.querySelector(".header__navigation-list");
+
+hamburgerMenu.addEventListener("click", () => {
+  navigation.style = "display: flex";
+  navigationList.style = "display: flex";
+});
+
+closeMenu.addEventListener("click", () => {
+  navigation.style = "display: none";
+});
